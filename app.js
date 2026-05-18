243bc2dce701aa9de39902f6305c8054de764e93
rajuyd-amalgamrx <drajashekar@amalgamrx.com>
2026-05-18 16:48:42 +0530

    add task definition json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD Pipeline - AWS ECS deployment working fine!');
+  res.send('CI/CD Pipeline - AWS ECS deployment working fine!!');
 });
 
 module.exports = app;
