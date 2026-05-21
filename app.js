e67c3d4afb7d49af5f25339f2a984b365f391652
rajuyd-amalgamrx <drajashekar@amalgamrx.com>
2026-05-21 16:50:27 +0530

    Added health check path

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
   res.send('CI/CD Pipeline - AWS ECS deployment - Images are tagged with git commit hash - Task definition is updated with new image URI');
 });
 
+app.get('/health', (req, res) => {
+  res.json({ ok: true });
+});
+
 module.exports = app;
